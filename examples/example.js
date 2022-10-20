b59b08f4ff1fe113bbf5be8fb3d07b76d469fd37
Harris <acyclicgraphs@gmail.com>
2022-10-20 12:41:15 -0400

    Update example.js

--- examples/example.js
+++ examples/example.js
@@ -1,1 +1,13 @@
+<script src="https://cdn.jsdelivr.net/npm/ipfs/dist/index.min.js"></script>
+<script>
+document.addEventListener('DOMContentLoaded', async () => {
+  const node = await Ipfs.create()
+  const results = await node.add('=^.^= meow meow')
+  const cid = results[0].hash
+  console.log('CID created via ipfs.add:', cid)
+  const data = await node.cat(cid)
+  console.log('Data read back via ipfs.cat:', new TextDecoder().decode(data))
+})
+</script>
 
+// https://github.com/ipfs/js-ipfs/blob/master/docs/BROWSERS.md
