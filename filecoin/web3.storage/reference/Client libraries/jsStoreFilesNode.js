508b7b7c0635cec05164308a3ba3680ffc721853
Harris <acyclicgraphs@gmail.com>
2022-11-01 14:17:11 -0400

    Update jsStoreFilesNode.js

--- filecoin/web3.storage/reference/Client libraries/jsStoreFilesNode.js
+++ filecoin/web3.storage/reference/Client libraries/jsStoreFilesNode.js
@@ -1,3 +1,5 @@
+// in Node.js using the ```getFilesFromPath``` helper to lad ```File``` objects from a local path: 
+
 import { getFilesFromPath } from 'web3.storage';
 
 const files = await getFilesFromPath('./files');
